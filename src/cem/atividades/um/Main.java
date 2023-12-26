01c0755d452bccd177d8782eabc1a642a070de23
Charles <charlesthiagosantos@gmail.com>
2023-12-26 19:37:15 -0300

    feat : criação do projeto 100Exercicios

---
+++ src/cem/atividades/um/Main.java
@@ -0,0 +1,13 @@
+package cem.atividades.um;
+
+public class Main {
+    /*
+    Escreva um programa que solicite ao usuário dois números e exiba a soma, subtração
+    , multiplicação e divisão entre eles.
+     */
+    public static void main(String[] args) {
+        Soma operacoes = new Soma(10, 12);
+        operacoes.calculoOperacaoBasica();
+    }
+
+}
---
+++ src/cem/atividades/um/Soma.java
@@ -0,0 +1,26 @@
+package cem.atividades.um;
+
+public class Soma {
+
+    /*
+    Escreva um programa que solicite ao usuário dois números e exiba a soma, subtração,
+     multiplicação e divisão entre eles.
+     */
+
+    private int num01;
+
+    private int num02;
+
+    public Soma(int num01, int num02) {
+        this.num01 = num01;
+        this.num02 = num02;
+    }
+
+    public void calculoOperacaoBasica() {
+        System.out.println("Resultados");
+        System.out.printf("Soma = %d\n", (num01 + num02));
+        System.out.printf("Subtração = %d\n",(num01 - num02));
+        System.out.printf("Multiplicação = %d\n", (num01 * num02));
+        System.out.printf("Divisão = %d\n", (num01 / num02));
+    }
+}
